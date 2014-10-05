c49ac7895d64adb4cf5e858e6a4d8400d1d0e6b4
Cesar Andreu <cesarandreu@gmail.com>
2014-10-05 13:56:27 -0700

    fix angular

--- client/services/ws.js
+++ client/services/ws.js
@@ -1,44 +1,48 @@
 'use strict';
 
 angular.module('chainCheck')
-.service('devices', function ($http, $q) {
-  this.list = function () {
-    var deferred = $q.defer();
-    $http
-      .get('/api/devices')
-      .success(function (devices) {
-        deferred.resolve(devices);
-      })
-      .error(function (err) {
-        deferred.reject(err);
-      });
-    return deferred.promise;
+.service('ws', function ($window) {
+  var api = {events: {}};
+
+  var wsHost = ($window.document.location.origin || ($window.location.protocol + '//' + $window.location.host)).replace(/^http/, 'ws');
+
+  var ws = new WebSocket(wsHost);
+  $window.setInterval(function () {
+    ws.send('ping');
+  }, 1000 * 25); // keep-alive signal
+
+
+  // utilize jQuery's callbacks as an event system
+  function event() {
+    var callbacks = window.$.Callbacks();
+    return {
+      subscribe: function ($scope, fn) {
+        if (fn) {
+          // unsubscribe from event on controller destruction to prevent memory leaks
+          $scope.$on('$destroy', function () {
+            callbacks.remove(fn);
+          });
+        } else {
+          fn = $scope;
+        }
+        callbacks.add(fn);
+      },
+      unsubscribe: callbacks.remove,
+      publish: callbacks.fire
+    };
+  }
+
+  // websocket connected disconnected events
+  api.connected = event();
+  ws.onopen = function () {
+    api.connected.publish.apply(this, arguments);
+    $rootScope.$apply();
   };
 
-  this.get = function (params) {
-    params = params || {};
-    var deferred = $q.defer();
-
-    if (params.deviceId) {
-      $http
-        .get('/api/devices/' + params.deviceId)
-        .success(function (device) {
-          deferred.resolve(device);
-        })
-        .error(function (err) {
-          deferred.reject(err);
-        });
-    } else {
-      deferred.reject('device id required');
-    }
-
-    return deferred.promise;
-  };
-
-  this.status = function (id, value) {
-    return $http.put('/api/devices/' + id, {
-      check: value
-    });
+  api.disconnected = event();
+  ws.onclose = function () {
+    api.disconnected.publish.apply(this, arguments);
+    $rootScope.$apply();
   };
 
 
